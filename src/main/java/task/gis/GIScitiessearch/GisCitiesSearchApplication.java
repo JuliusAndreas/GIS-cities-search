7a6b7e69f188443023dcc46ea382d53b45f1da51
Soroosh Faal <faalsoroosh@gmail.com>
2023-12-20 15:49:32 +0330

    add project files

---
+++ src/main/java/task/gis/GIScitiessearch/GisCitiesSearchApplication.java
@@ -0,0 +1,13 @@
+package task.gis.GIScitiessearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisCitiesSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisCitiesSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/gis/GIScitiessearch/GisCitiesSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package task.gis.GIScitiessearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisCitiesSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
